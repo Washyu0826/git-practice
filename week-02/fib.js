ee35137b9fae8c2d1dd0f8bf22064ff9f65d931c
Washyu0826 <washyu12345678@gmail.com>
2024-09-23 21:43:54 +0800

    Add files via upload

---
+++ week-02/fib.js
@@ -0,0 +1,11 @@
+function fib(n) {
+    if (n === 1) {return 1;}
+    else if (n === 2) {return 1;}
+    else {return fib(n-1)+fib(n-2);}
+  }
+  
+  console.log(fib(1)); 
+  console.log(fib(2)); 
+  console.log(fib(5));
+  console.log(fib(10));
+  
