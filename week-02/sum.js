ef44480d665d78326cbbdde7bded1536fd8d21a0
Washyu0826 <washyu12345678@gmail.com>
2024-09-23 22:26:11 +0800

    Update sum.js

--- week-02/sum.js
+++ week-02/sum.js
@@ -1,3 +1,4 @@
+// 加總輸入陣列元素之函式
 // 法一
 function sum1(ary) {
 	return ary.reduce((acc,val) => {
@@ -6,9 +7,14 @@ function sum1(ary) {
 
 console.log(sum1([1, 5, 3, 2])); // 11
 
-//法二
+// 法二
 const sum2 = (ary) => {
     return ary.reduce((acc,val) => acc +val,0);
   }
     
 console.log(sum2([1, 5, 3, 7])); // 16
+
+// 輸入為n，輸出1+2+..n 之加總函式
+function sumtotal(n) {
+    return (n*(n+1)/2);
+}
