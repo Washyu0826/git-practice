ba1fa0c97a5960005db2399a2aa2b716e36495b6
Washyu0826 <washyu12345678@gmail.com>
2024-09-27 19:52:47 +0800

    Update main.js

--- week-02/main.js
+++ week-02/main.js
@@ -1,12 +1,34 @@
-// main.js
-// TODO 以 Module 的方式匯入，例如:
-import Stack from "./stack.mjs";
-let stack = new Stack();
-stack.print();
+import Stack from "./stack.js";
 
-stack.push(5);
-stack.push(8);
-stack.print();
+const testStack = new Stack(3);
 
-// TODO: 應該還要做哪些測試，以驗證自己開發的 stack 是沒有問題的？
+// 1.測試容量
+try {
+    testStack.push(1);
+    testStack.push(2);
+    testStack.push(3);
+    testStack.push(4); // 超過容量錯誤
+} catch (e) {
+    console.log(e.message); // "Stack overflow"
+}
+
+console.log("Current stack:");
+testStack.print(); //  [1, 2, 3]
+
+// 2.測試元素類型
+try {
+    testStack.push("four"); // 不支援字串
+} catch (e) {
+    console.log(e.message); // "Type Error"
+}
+
+// 3.測試完整性
+console.log("Current stack size:", testStack.size()); // 3
+console.log("Contains 2?", testStack.contains(2)); // true
+console.log("Contains 5?", testStack.contains(5)); // false
+
+// 4.測試 toArray 方法
+console.log("Stack as array:", testStack.toArray()); //  [1, 2, 3]
+
+
 
