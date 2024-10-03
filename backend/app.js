e1258ffac52333048ecaa6051e69d32cdf374b80
Washyu0826 <washyu12345678@gmail.com>
2024-10-03 12:22:26 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,3 +9,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+// cmd : $env:PORT=4000
+// cmd : node app.js
+// Server is listening on port 4000
