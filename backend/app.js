ae9af256d0a6ccf3d5c5120991ebcaf47ad531b2
Washyu0826 <washyu12345678@gmail.com>
2024-10-31 13:03:20 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 console.log('Loading app.js');
 const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 const port = process.env.PORT || 3000;
 
